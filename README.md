c9137893a1aeba3415503fae1af6520c7be9f6bd
Ju Nan <junan76@163.com>
2026-05-19 22:53:01 +0800

    core: cpu, 8-bit load instructions done.
    Signed-off-by: Ju Nan <junan76@163.com>

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # gamebox
+
+A cross platform gameboy emulator.
+
+## CPU Instruction Set
+
+### 8-bit load instructions
+
+- [x] ld r8, r8
+- [x] ld r8, n8
+- [x] ld r8, [hl]
+- [x] ld [hl], r8
+- [x] ld [hl], n8
+- [x] ld a, [r16]
+- [x] ld [r16], a
+- [x] ld a, [n16]
+- [x] ld [n16], a
+- [x] ldh a, [c]
+- [x] ldh [c], a
+- [x] ldh a, [n16]
+- [x] ldh [n16], a
+- [x] ld a, [hl+]
+- [x] ld [hl+], a
+- [x] ld a, [hl-]
+- [x] ld [hl-], a
+
+### 16-bit load instructions
+
+- [x] ld r16, n16
+- [ ] ld [n16], sp
+- [ ] ld sp, hl
+- [ ] push r16
+- [ ] pop r16
+- [ ] ld hl, sp+e8
+
+### 8-bit arithmetic and logical instructions
+
+### 16-bit arithmetic instructions
+
+### Control flow instructions
+
+### Miscellaneous instructions
--- core/cpu.c
+++ core/cpu.c
@@ -96,10 +96,15 @@ static uint8_t *cpu_reg8(uint8_t encode)
     }
 }
 
+static uint8_t ill_op(uint8_t opcode)
+{
+    cpu->stopped = 1;
+    return 0;
+}
+
 /*
  * 8-bit load instructions.
  */
-
 static uint8_t ld_r8_r8(uint8_t opcode)
 {
     uint8_t *rs, *rd;
@@ -107,7 +112,6 @@ static uint8_t ld_r8_r8(uint8_t opcode)
     rd = cpu_reg8((opcode >> 3) & 0x7);
 
     *rd = *rs;
-    cpu->regs.pc++;
     return 4;
 }
 
@@ -119,14 +123,192 @@ static uint8_t ld_r8_n8(uint8_t opcode)
     rd = cpu_reg8((opcode >> 3) & 0x7);
     n8 = bus_read8(cpu->regs.pc++);
 
-    if (rd != NULL)
-    {
-        *rd = n8;
-        return 8;
-    }
-    else
-    {
-        bus_write8(cpu->regs.hl, n8);
-        return 12;
-    }
+    *rd = n8;
+    return 8;
 }
+
+static uint8_t ld_r8_hl(uint8_t opcode)
+{
+    uint8_t *rd = cpu_reg8((opcode >> 3) & 0x7);
+    *rd = bus_read8(cpu->regs.hl);
+    return 8;
+}
+
+static uint8_t ld_hl_r8(uint8_t opcode)
+{
+    uint8_t *rs = cpu_reg8(opcode & 0x7);
+    bus_write8(cpu->regs.hl, *rs);
+    return 8;
+}
+
+static uint8_t ld_hl_n8(uint8_t opcode)
+{
+    uint8_t value = bus_read8(cpu->regs.pc++);
+    bus_write8(cpu->regs.hl, value);
+    return 12;
+}
+
+static uint8_t ld_a_r16(uint8_t opcode)
+{
+    if (opcode == 0x0a)
+    {
+        cpu->regs.a = bus_read8(cpu->regs.bc);
+    }
+    else if (opcode == 0x1a)
+    {
+        cpu->regs.a = bus_read8(cpu->regs.de);
+    }
+    return 8;
+}
+
+static uint8_t ld_r16_a(uint8_t opcode)
+{
+    if (opcode == 0x02)
+    {
+        bus_write8(cpu->regs.bc, cpu->regs.a);
+    }
+    else if (opcode == 0x12)
+    {
+        bus_write8(cpu->regs.de, cpu->regs.a);
+    }
+    return 8;
+}
+
+static uint8_t ld_a_n16(uint8_t opcode)
+{
+    uint16_t addr = bus_read16(cpu->regs.pc);
+    cpu->regs.a = bus_read8(addr);
+    cpu->regs.pc += 2;
+    return 16;
+}
+
+static uint8_t ld_n16_a(uint8_t opcode)
+{
+    uint16_t addr = bus_read16(cpu->regs.pc);
+    bus_write8(addr, cpu->regs.a);
+    cpu->regs.pc += 2;
+    return 16;
+}
+
+static uint8_t ldh_a_c(uint8_t opcode)
+{
+    cpu->regs.a = bus_read8(0xFF00 + cpu->regs.c);
+    return 8;
+}
+
+static uint8_t ldh_c_a(uint8_t opcode)
+{
+    bus_write8(0xFF00 + cpu->regs.c, cpu->regs.a);
+    return 8;
+}
+
+static uint8_t ldh_a_n16(uint8_t opcode)
+{
+    uint8_t low = bus_read8(cpu->regs.pc);
+    uint16_t addr = 0xFF00 + low;
+    cpu->regs.a = bus_read8(addr);
+    cpu->regs.pc++;
+    return 12;
+}
+
+static uint8_t ldh_n16_a(uint8_t opcode)
+{
+    uint8_t low = bus_read8(cpu->regs.pc);
+    uint16_t addr = 0xFF00 + low;
+    bus_write8(addr, cpu->regs.a);
+    cpu->regs.pc++;
+    return 12;
+}
+
+static uint8_t ldi_a_hl(uint8_t opcode)
+{
+    cpu->regs.a = bus_read8(cpu->regs.hl++);
+    return 8;
+}
+
+static uint8_t ldi_hl_a(uint8_t opcode)
+{
+    bus_write8(cpu->regs.hl++, cpu->regs.a);
+    return 8;
+}
+
+static uint8_t ldd_a_hl(uint8_t opcode)
+{
+    cpu->regs.a = bus_read8(cpu->regs.hl--);
+    return 8;
+}
+
+static uint8_t ldd_hl_a(uint8_t opcode)
+{
+    bus_write8(cpu->regs.hl--, cpu->regs.a);
+    return 8;
+}
+
+/*
+ * DOING:
+ * 16-bit load instructions.
+ */
+static uint8_t ld_r16_n16(uint8_t opcode)
+{
+    uint16_t value = bus_read16(cpu->regs.pc);
+
+    switch (opcode)
+    {
+    case 0x01:
+        cpu->regs.bc = value;
+        break;
+    case 0x11:
+        cpu->regs.de = value;
+        break;
+    case 0x21:
+        cpu->regs.hl = value;
+        break;
+    case 0x31:
+        cpu->regs.sp = value;
+        break;
+    }
+
+    cpu->regs.pc += 2;
+    return 12;
+}
+
+/*
+ * TODO:
+ * 8-bit arithmetic and logical instructions.
+ */
+
+/*
+ * TODO:
+ * 16-bit arithmetic instructions.
+ */
+
+/*
+ * TODO:
+ * Control flow instructions.
+ */
+
+/*
+ * TODO:
+ * Miscellaneous instructions.
+ */
+
+typedef uint8_t (*opcode_handler)(uint8_t);
+opcode_handler opcode_handlers[256] = {
+    ill_op, /*0x00 */
+    ill_op, /*0x01 */
+    ill_op, /*0x02 */
+    ill_op, /*0x03 */
+    ill_op, /*0x04 */
+    ill_op, /*0x05 */
+    ill_op, /*0x06 */
+    ill_op, /*0x07 */
+    ill_op, /*0x08 */
+    ill_op, /*0x09 */
+    ill_op, /*0x0a */
+    ill_op, /*0x0b */
+    ill_op, /*0x0c */
+    ill_op, /*0x0d */
+    ill_op, /*0x0e */
+    ill_op, /*0x0f */
+
+};
